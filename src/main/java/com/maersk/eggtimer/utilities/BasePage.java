f01f9cdbd360379cd8f235cae94baca9047a4571
kkareemk <kareemk.it@gmail.com>
2021-09-15 00:01:57 +0530

    updated details

--- src/main/java/com/maersk/eggtimer/utilities/BasePage.java
+++ src/main/java/com/maersk/eggtimer/utilities/BasePage.java
@@ -28,7 +28,7 @@ public class BasePage {
             options.addArguments("--no-sandbox");
             options.addArguments("--start-maximized");
             options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--headless");
+            options.setExperimentalOption("useAutomationExtension", false);
             driver= new ChromeDriver(options);
         }else{
             System.out.println("This is not valid browser, please enter valid browser names - firefox or chrome");
