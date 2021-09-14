1f9d705dad7752012f2d4780d470f2506c9ecc9c
kkareemk <kareemk.it@gmail.com>
2021-09-14 23:33:48 +0530

    updated details

--- src/main/java/com/maersk/eggtimer/runner/BaseRunnerTest.java
+++ src/main/java/com/maersk/eggtimer/runner/BaseRunnerTest.java
@@ -2,10 +2,9 @@ package com.maersk.eggtimer.runner;
 
 
 import com.maersk.eggtimer.utilities.BasePage;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.AfterClass;
+import org.junit.After;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
@@ -17,8 +16,9 @@ import org.junit.runner.RunWith;
 )
 
 public class BaseRunnerTest {
-    @AfterClass
+    @After
     public static void tearDown(){
+        System.out.println("inside teardown");
         BasePage.tearDown();
     }
 }
--- src/main/java/com/maersk/eggtimer/utilities/BasePage.java
+++ src/main/java/com/maersk/eggtimer/utilities/BasePage.java
@@ -1,7 +1,6 @@
 package com.maersk.eggtimer.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
